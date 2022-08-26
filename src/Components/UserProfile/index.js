fedc25111aa920e7b840d419fc5be478f4ba198b
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-26 15:52:13 +0100

    changed img width to not pixels

--- src/Components/UserProfile/index.js
+++ src/Components/UserProfile/index.js
@@ -3,7 +3,7 @@ import User from './person-circle.svg'
 const UserProfile = () => {
     return (
         <>
-            <img src={User} width="60px"/>
+            <img src={User} width="60"/>
             <h5 class="align-self-center mb-0 px-3">User Name</h5>
         </>
     )
