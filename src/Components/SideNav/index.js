32c846718b988a62a232102bdafac0775af0896d
erc-clarkson <erc.clarkson@btinternet.com>
2022-08-26 16:02:31 +0100

    side nav bar with inbox link, non functional links

---
+++ src/Components/SideNav/index.js
@@ -0,0 +1,16 @@
+import EmailNavLinks from '../EmailNavLinks';
+import './styles.scss';
+
+const SideNav = () => {
+
+    return (
+        <>
+       
+<div className="sideNavContainer">
+    <EmailNavLinks />
+</div>
+        </>
+    )
+}
+
+export default SideNav
