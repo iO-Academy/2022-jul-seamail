82db8b3cf87f3260f117f3a90a880179e880060a
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-26 15:50:24 +0100

    changed col sizes at small screens

--- src/Components/TopNav/index.js
+++ src/Components/TopNav/index.js
@@ -5,10 +5,10 @@ const TopNav = () => {
     return (
         <div className="container">
             <nav className="row navbar bg-secondary">
-                <div className="col-2 col-lg-6 d-flex justify-content-start align-self-center d-lg-none">
+                <div className="col-3 col-lg-6 d-flex justify-content-start align-self-center d-lg-none">
                     <MobileMenuButton />
                 </div>
-                <div className="col-6 col-lg-6 d-flex justify-content-start align-self-center">
+                <div className="col-5 col-lg-6 d-flex justify-content-start align-self-center">
                     <h1 className="mb-0">SeaMail</h1>
                 </div>
                 <div className="col-4 col-lg-6 d-flex justify-content-end">
