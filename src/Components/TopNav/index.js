9c136ebb597c3cf4fb53ab3036e435e37b22545f
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-26 15:37:39 +0100

    added user svg and changed fixed sizes,

--- src/Components/TopNav/index.js
+++ src/Components/TopNav/index.js
@@ -4,14 +4,11 @@ import UserProfile from "../UserProfile"
 const TopNav = () => {
     return (
         <div className="container">
-            <nav className="row navbar navbar-expand-lg bg-secondary">
-                <div className="col-12 col-lg-6">
-                    <MobileMenuButton />
-                </div>
-                <div className="col-12 col-lg-6">
+            <nav className="row navbar bg-secondary">
+                <div className="col-4 col-lg-6 d-flex justify-content-start align-self-center">
                     <h1>SeaMail</h1>
                 </div>
-                <div className="col-12 col-lg-6">
+                <div className="col-4 col-lg-6 d-flex justify-content-end">
                     <UserProfile />
                 </div>
             </nav>
--- src/Components/UserProfile/index.js
+++ src/Components/UserProfile/index.js
@@ -1,7 +1,10 @@
+import User from './person-circle.svg'
+
 const UserProfile = () => {
     return (
         <>
-            
+            <img src={User} width="60px"/>
+            <h5 class="align-self-center mb-0 px-3">User Name</h5>
         </>
     )
 }
