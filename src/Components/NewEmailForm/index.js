6ba855f6c57e36051652cab4f11d712ba28fe0e9
cr-walton <cxw507@gmail.com>
2022-08-31 12:13:06 +0100

    email form validation added, error messages added

--- src/Components/NewEmailForm/index.js
+++ src/Components/NewEmailForm/index.js
@@ -1,16 +1,46 @@
+import { useState } from "react"
+
 const NewEmailForm = ({setSideNavVisible, sideNavVisible}) => {
+
+    const [emailAddressValid, setEmailAddressValid] = useState(null)
+    const [emailSubjectValid, setEmailSubjectValid] = useState(null)
+    
+    const validEmail =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+    const validateEmail = (e) => {
+        let emailAdresses = e.target.value.split(' ')
+            emailAdresses.forEach(emailAddress => {
+                if(!emailAddress.match(validEmail) || emailAddress == '' ) {
+                    setEmailAddressValid(false) 
+                   
+                } else {
+                    setEmailAddressValid(true)
+                }
+                console.log(emailAddressValid)
+            })
+        }
+    
+    const validateSubject = (e) => {
+        if(e.target.value === ''){
+            setEmailSubjectValid(false)
+        } else {
+            setEmailSubjectValid(true)
+        }
+    }
 
     return (
 
         <form className="col bg-white border p-5">
             <div className="mb-4">
-                <input className="col form-control" name="to" type="text" id="to" placeholder="To"></input>
+                <input onBlur={validateEmail} className="col form-control mb-2" name="to" type="text" id="to" placeholder="To"></input>
+                <span className='text-danger'>{emailAddressValid === false ? "Invalid email address. Multiple addresses must be seperated by a space" : ''}</span>
             </div>
             <div className="mb-4">
-                <input className="col form-control" name="subject" type="text" id="subject" placeholder="Subject"></input>
+                <input onBlur={validateSubject} className="col form-control" name="subject" type="text" id="subject" placeholder="Subject"></input>
+                <span className='text-danger'>{emailSubjectValid === false ? 'Email subject empty, send anyway?' : ''}</span>
             </div>
             <div className="input-group mb-4">
-                <textarea class="form-control" aria-label="With textarea" placeholder="Write your email here"></textarea>
+                <textarea className="form-control" aria-label="With textarea" placeholder="Write your email here"></textarea>
             </div>
         </form>
     )
