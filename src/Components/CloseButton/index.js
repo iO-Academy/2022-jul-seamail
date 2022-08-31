54e440c1aa1c12072983c785d278a4876beb1075
Jdixon04 <jdixon0437@googlemail.com>
2022-08-31 16:13:35 +0100

    made readability changes

--- src/Components/CloseButton/index.js
+++ src/Components/CloseButton/index.js
@@ -2,12 +2,10 @@
 const CloseButton = ({emailDisplayVisible, setEmailDisplayVisible}) => {
 
     const handleClick = () => {
-
         setEmailDisplayVisible(!emailDisplayVisible)
     } 
-
+    
     return <button onClick={handleClick} type="button" className="btn-close" aria-label="Close"></button>
-
 }
 
 export default CloseButton
