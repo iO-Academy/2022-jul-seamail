742e0c86acbd9373a5ff1ce1a7bf326f6cc1a46c
erc-clarkson <erc.clarkson@btinternet.com>
2022-08-31 16:31:17 +0100

    new email nav link background color turns to blue with the click event

--- src/Components/NewEmailNavLink/index.js
+++ src/Components/NewEmailNavLink/index.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react"
 
 const NewEmailNavLink = ({newEmailVisible, setNewEmailVisible}) => {
+    const [isActive, setIsActive] = useState(false);
 
     const handleClick = () => {
         setNewEmailVisible(true)
-        console.log(newEmailVisible)
+        setIsActive(true);
     }
 
     return (
         <>
-        <div className="row py-3 ps-2">
+        <div className="row py-3 ps-2" 
+        style={{
+          backgroundColor: isActive ? '#007bfe' : '',
+        }}>
             <a onClick={handleClick} className="fw-bold text-decoration-none">New Email</a>
         </div>
         </>
