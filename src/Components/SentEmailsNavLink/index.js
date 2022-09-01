09c74ee21190c832a0993b407da2a9e017d3fc11
erc-clarkson <erc.clarkson@btinternet.com>
2022-09-01 10:12:40 +0100

    more information in new component

--- src/Components/SentEmailsNavLink/index.js
+++ src/Components/SentEmailsNavLink/index.js
@@ -1,9 +1,16 @@
 const SentEmailsNavLink = () => {
+    // const [isActive, setIsActive] = useState(false);
 
+    // const handleClick = () => {
+    //     setNewEmailVisible(true)
+    //     setIsActive(true);
+    // }
 
     return (
         <>
-        
+        <div className="row ps-2">
+            <a className="fw-bold text-decoration-none">Sent</a>
+        </div>
         </>
     )
 }
